af1d90b0d9cba03235c696b84b7d553587738f4c
Matt Zumwalt <matt@curationexperts.com>
2014-05-04 21:43:42 -0500

    Makes Creating image works roughly identical to creating Text works

--- app/repository_models/image.rb
+++ app/repository_models/image.rb
@@ -13,38 +13,13 @@ class Image < ActiveFedora::Base
   
   self.accept_datastream_attachments ["VRA", "MODS"]
   
+  include CurationConcern::WithCaseBasicMetadata
   has_metadata "descMetadata", type: GenericWorkRdfDatastream
 
   include CurationConcern::RemotelyIdentifiedByDoi::Attributes
   
   class_attribute :human_readable_short_description
-  self.human_readable_short_description = "Any Text work, preferably with TEI xml attached."
-  
-  attribute :title, datastream: :descMetadata,
-    multiple: false,
-    validates: {presence: { message: 'Your work must have a title.' }}
-
-  attribute :rights, datastream: :descMetadata,
-    multiple: false,
-    validates: {presence: { message: 'You must select a license for your work.' }}
-
-  attribute :created,        datastream: :descMetadata, multiple: false
-  attribute :description,    datastream: :descMetadata, multiple: false
-  attribute :date_uploaded,  datastream: :descMetadata, multiple: false
-  attribute :date_modified,  datastream: :descMetadata, multiple: false
-  attribute :available,      datastream: :descMetadata, multiple: false
-  attribute :creator,        datastream: :descMetadata, multiple: false
-  attribute :content_format, datastream: :descMetadata, multiple: false
-  attribute :identifier,     datastream: :descMetadata, multiple: false
-
-  attribute :contributor,            datastream: :descMetadata, multiple: true
-  attribute :publisher,              datastream: :descMetadata, multiple: true
-  attribute :bibliographic_citation, datastream: :descMetadata, multiple: true
-  attribute :source,                 datastream: :descMetadata, multiple: true
-  attribute :language,               datastream: :descMetadata, multiple: true
-  attribute :extent,                 datastream: :descMetadata, multiple: true
-  attribute :requires,               datastream: :descMetadata, multiple: true
-  attribute :subject,                datastream: :descMetadata, multiple: true
+  self.human_readable_short_description = "Any Image work, preferably with VRA xml attached."
 
   attribute :files, multiple: true, form: {as: :file},
     hint: "CTRL-Click (Windows) or CMD-Click (Mac) to select multiple files."
