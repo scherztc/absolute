d58cddd50678efa10a26bdda37ad6dafd5b8c34d
val99erie <val99erie@gmail.com>
2014-05-14 15:09:57 -0500

    Use blacklight onLoad instead of document ready to work more nicely with turbolinks.  Story #64

--- app/assets/javascripts/sufia/fileupload.js
+++ app/assets/javascripts/sufia/fileupload.js
@@ -1,6 +1,7 @@
 //= require sufia/uploader
 // This file is the default initialization of the fileupload.  If you want to call
 // sufiaUploader with other options (like afterSubmit), then override this file.
-$(function() {
+
+Blacklight.onLoad(function() {
   $('#fileupload').sufiaUploader();
 });
