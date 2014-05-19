c9fedf453d323e4c7caf3c453bf621602548e6a1
Matt Zumwalt <matt@curationexperts.com>
2014-05-19 10:53:13 -0500

    Makes dc:format multi-valued.

--- app/repository_models/curation_concern/with_case_basic_metadata.rb
+++ app/repository_models/curation_concern/with_case_basic_metadata.rb
@@ -26,12 +26,12 @@ module CurationConcern::WithCaseBasicMetadata
     validates_presence_of :title, message: 'Your work must have a title.' 
       
     # Single-value fields
-    has_attributes :available,:created,:content_format,:date_modified,
+    has_attributes :available,:created,:date_modified,
                    :date_uploaded,:description,:rights,:title,
                 datastream: :descMetadata, multiple: false
                 
     # Multi-value fields
-    has_attributes :contributor, :creator, :contributors,:coverage,:date,:extent,:identifier,:language,
+    has_attributes :contributor, :creator, :contributors,:coverage,:date,:extent,:content_format,:identifier,:language,
                    :publisher,:relation,:requires,:rights,:source,:subject,:type,
                 datastream: :descMetadata, multiple: true
   end
