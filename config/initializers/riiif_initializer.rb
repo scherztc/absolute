0e2fb9d797b15ea82b41a427c7ea579f804eda54
Justin Coyne <justin@curationexperts.com>
2014-05-20 17:14:24 -0500

    Use the cached height/width from solr for IIIF

--- config/initializers/riiif_initializer.rb
+++ config/initializers/riiif_initializer.rb
@@ -1,4 +1,13 @@
 Riiif::Image.file_resolver = Riiif::HTTPFileResolver
+Riiif::Image.info_service = lambda do |id, file|
+  resp = get_solr_response_for_doc_id id
+  doc = resp.first['response']['docs'].first
+  { height: doc['height_isi'], width: doc['width_isi'] }
+end
+include Blacklight::SolrHelper
+def blacklight_config
+  CatalogController.blacklight_config
+end
 
 Riiif::HTTPFileResolver.id_to_uri = lambda do |id| 
   pid = Sufia::Noid.namespaceize(id)
