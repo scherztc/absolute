b452381b5c40b5ea5267c44d8b09ddcbea7ea572
val99erie <val99erie@gmail.com>
2014-06-02 15:05:19 -0500

    Revert "Changed the wording of authenticated visibility.  Story #151" This reverts commit 0c091c6bc6e4a149be4f9fe935b172d4651c56a1.

--- app/helpers/visibility_helper.rb
+++
@@ -1,9 +0,0 @@
-module VisibilityHelper
-  def human_readable_visibility(visibility)
-    if visibility == Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_AUTHENTICATED
-      'Case Western Reserve University'
-    else
-      visibility
-    end
-  end
-end
