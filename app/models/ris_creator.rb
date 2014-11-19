7a9ab8a68a13b75c90503cb760a5fd4661b84111
Joe Fennimore <jrf118@case.edu>
2014-11-19 12:04:01 -0500

    ris_creator_spec works and runs three different tests, though the conditions are very similar in most possible calls to the ris_creator. I also cleaned out ris_creator, removing some methods and comments that werent being used or giving important information.
    Signed-off-by: Joe Fennimore <jrf118@case.edu>

--- app/models/ris_creator.rb
+++ app/models/ris_creator.rb
@@ -19,29 +19,63 @@
 # the BentoSearch::ResultItem.
 class RISCreator 
 	def initialize(i)
-		# 'i' is the Text object that is returned by the rails console command ActiveFedora::Base.find('pid').
-		# The Text object has information relevant to creating the ris citation.
+		# 'i' is the Text object that is returned by ActiveFedora::Base.find('pid').
 		@item = i
+<<<<<<< HEAD
 
 		#TODO The type method for @item is returning this error:
 		# 'Tried to fetch `type' from solr, but it isn't indexed.'
 		# @ris_format = translate_ris_format
+=======
+		@ris_format = translate_ris_format
+>>>>>>> c5cbd31... Started on the spec file to test the RISCreator and also cleaned out some of the sections of the RISCreator that were commented out from the bentosearch.  In the spec, Im attempting to create a test GenericWork to then run the export on and test the output with line by line comparison.
 	end
 
 	def export
 		out = "".force_encoding("UTF-8")
 		#out << tag_format("TY", @ris_format)
 
+<<<<<<< HEAD
 		id = @item.to_param
 		out << tag_format("ID", id)
+=======
+		# @item.to_param returns an id that always starts with "ksl:"
+		# For the RIS citation we want to remove that, though, and only return the pid.		
+		id = @item.id.to_s.split ":"
+		out << tag_format("ID", id[1])
+>>>>>>> 0457a3b... ris_creator_spec works and runs three different tests, though the conditions are very similar in most possible calls to the ris_creator. I also cleaned out ris_creator, removing some methods and comments that werent being used or giving important information.
 
+<<<<<<< HEAD
 		out << tag_format("TI", @item.title)		
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+=======
+>>>>>>> c375d0c... Started on the spec file to test the RISCreator and also cleaned out some of the sections of the RISCreator that were commented out from the bentosearch.  In the spec, Im attempting to create a test GenericWork to then run the export on and test the output with line by line comparison.
+=======
+		# @item.title returns something like  ["Title of this item"]
+		# So to remove the surrounding pairs of quotes and brackets using split
+		title = @item.title.to_s.split "\""
+<<<<<<< HEAD
+<<<<<<< HEAD
+		out << tag_format("TI", title[1])		
+>>>>>>> c5cbd31... Started on the spec file to test the RISCreator and also cleaned out some of the sections of the RISCreator that were commented out from the bentosearch.  In the spec, Im attempting to create a test GenericWork to then run the export on and test the output with line by line comparison.
+=======
+		out << tag_format("TI", title[1])
+		
+>>>>>>> 0457a3b... ris_creator_spec works and runs three different tests, though the conditions are very similar in most possible calls to the ris_creator. I also cleaned out ris_creator, removing some methods and comments that werent being used or giving important information.
+>>>>>>> a0e8b9a... ris_creator_spec works and runs three different tests, though the conditions are very similar in most possible calls to the ris_creator. I also cleaned out ris_creator, removing some methods and comments that werent being used or giving important information.
+=======
+		out << tag_format("TI", title[1])		
+>>>>>>> c5cbd31... Started on the spec file to test the RISCreator and also cleaned out some of the sections of the RISCreator that were commented out from the bentosearch.  In the spec, Im attempting to create a test GenericWork to then run the export on and test the output with line by line comparison.
+>>>>>>> c375d0c... Started on the spec file to test the RISCreator and also cleaned out some of the sections of the RISCreator that were commented out from the bentosearch.  In the spec, Im attempting to create a test GenericWork to then run the export on and test the output with line by line comparison.
 		@item.creator.each do |author|		
 			out << tag_format("AU", author)
 		end
 		@item.contributor.each do |author|
 			out << tag_format("AU", author)
 		end
+<<<<<<< HEAD
 			#out << tag_format("PY", @item.year)
 			# We have the date rather than just the year
 		out << tag_format("DA", @item.date)
@@ -52,19 +86,39 @@ class RISCreator
 			#out << tag_format("IS", @item.issue)
 			#out << tag_format("SP", @item.start_page)
 			#out << tag_format("EP", @item.end_page)
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+=======
+		date = @item.date.to_s.split "\""
+		out << tag_format("DA", date[1])
+<<<<<<< HEAD
+>>>>>>> c5cbd31... Started on the spec file to test the RISCreator and also cleaned out some of the sections of the RISCreator that were commented out from the bentosearch.  In the spec, Im attempting to create a test GenericWork to then run the export on and test the output with line by line comparison.
+>>>>>>> a0e8b9a... ris_creator_spec works and runs three different tests, though the conditions are very similar in most possible calls to the ris_creator. I also cleaned out ris_creator, removing some methods and comments that werent being used or giving important information.
+=======
+=======
+		date = @item.date.to_s.split "\""
+		out << tag_format("DA", date[1])
+>>>>>>> c5cbd31... Started on the spec file to test the RISCreator and also cleaned out some of the sections of the RISCreator that were commented out from the bentosearch.  In the spec, Im attempting to create a test GenericWork to then run the export on and test the output with line by line comparison.
+>>>>>>> c375d0c... Started on the spec file to test the RISCreator and also cleaned out some of the sections of the RISCreator that were commented out from the bentosearch.  In the spec, Im attempting to create a test GenericWork to then run the export on and test the output with line by line comparison.
 		
 		#TODO The source method is also returning the index error:
 		# 'Tried to fetch `source' from solr, but it isn't indexed.'	
 		# @item.source.each do |source_title|		
 			# out << tag_format("T2", source_title)
 		# end
-			# ISSN and ISBN both share SN
-			# And we don't have methods to find either
-			#out << tag_format("SN", @item.issn)
-			#out << tag_format("SN", @item.isbn)
-			#out << tag_format("DO", @item.doi)	
+	
 		out << tag_format("PB", @item.publisher)
 		out << tag_format("AB", @item.description)
+=======
+	
+		pub = @item.publisher.to_s.split "\""
+		out << tag_format("PB", pub[1])
+
+		desc = @item.description.to_s.split "\""
+		out << tag_format("AB", desc[1])
+
+>>>>>>> 0457a3b... ris_creator_spec works and runs three different tests, though the conditions are very similar in most possible calls to the ris_creator. I also cleaned out ris_creator, removing some methods and comments that werent being used or giving important information.
 		@item.subject.each do |kw|
 			out << tag_format("KW", kw)
 		end
@@ -72,12 +126,31 @@ class RISCreator
 		relation = @item.relation.to_s.split"\""		
 		out << tag_format("LB", relation[1])	
 		# include main link and any other links?
+<<<<<<< HEAD
 		out << tag_format("UR", @item.identifier)
 		# @item.linked_resource_urls.each do |link|
 			# out << tag_format("UR", link.url)
 		# end
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+=======
+		url = @item.identifier.to_s.split "\""
+		out << tag_format("UR", url[1])
+<<<<<<< HEAD
+>>>>>>> c5cbd31... Started on the spec file to test the RISCreator and also cleaned out some of the sections of the RISCreator that were commented out from the bentosearch.  In the spec, Im attempting to create a test GenericWork to then run the export on and test the output with line by line comparison.
+>>>>>>> a0e8b9a... ris_creator_spec works and runs three different tests, though the conditions are very similar in most possible calls to the ris_creator. I also cleaned out ris_creator, removing some methods and comments that werent being used or giving important information.
+=======
+=======
+		url = @item.identifier.to_s.split "\""
+		out << tag_format("UR", url[1])
+>>>>>>> c5cbd31... Started on the spec file to test the RISCreator and also cleaned out some of the sections of the RISCreator that were commented out from the bentosearch.  In the spec, Im attempting to create a test GenericWork to then run the export on and test the output with line by line comparison.
+>>>>>>> c375d0c... Started on the spec file to test the RISCreator and also cleaned out some of the sections of the RISCreator that were commented out from the bentosearch.  In the spec, Im attempting to create a test GenericWork to then run the export on and test the output with line by line comparison.
 		# end with blank lines, so multiple ones can be concatenated for
 		# a file.
+=======
+		# end with blank lines, so multiple ones can be concatenated for a file.
+>>>>>>> 0457a3b... ris_creator_spec works and runs three different tests, though the conditions are very similar in most possible calls to the ris_creator. I also cleaned out ris_creator, removing some methods and comments that werent being used or giving important information.
 		out << "\r\nER - \r\n\r\n"
 	end
 
@@ -126,12 +199,6 @@ class RISCreator
 	#
 	# "Note, however, that the asterisk (character 42)
 	# is not allowed in the author, keywords or periodical name fields."
-	#
-	# The spec also seems to say ascii-only, but I don't think that's true
-	# for actually existing software, we do utf-8.
-	#
-	# Refworks MAY require unicode composed normalization if it accepts utf8
-	# at all. but not doing that yet. http://bibwild.wordpress.com/2010/04/28/refworks-problems-importing-diacritics/
 	def tag_format(tag, value)
 		return "" if value.blank?
 		raise ArgumentError.new("Illegal RIS tag") unless tag =~ /[A-Z][A-Z0-9]/
@@ -144,6 +211,7 @@ class RISCreator
 		return "\r\n#{tag} - #{value}"
 	end
 
+<<<<<<< HEAD
 	# Take a ruby Date and translate to RIS date format
 	# "YYYY/MM/DD/other info"
 	#
@@ -153,6 +221,7 @@ class RISCreator
 		return d.strftime("%Y/%m/%d")
 	end
 
+<<<<<<< HEAD
 	# RIS wants `Last, First M.`
 	# All of the authors being stored in Digital Case are already in this format though.
 	def format_author_name(author)
@@ -173,4 +242,8 @@ class RISCreator
 		end
 	end
 
+=======
+>>>>>>> a0e8b9a... ris_creator_spec works and runs three different tests, though the conditions are very similar in most possible calls to the ris_creator. I also cleaned out ris_creator, removing some methods and comments that werent being used or giving important information.
+=======
+>>>>>>> c375d0c... Started on the spec file to test the RISCreator and also cleaned out some of the sections of the RISCreator that were commented out from the bentosearch.  In the spec, Im attempting to create a test GenericWork to then run the export on and test the output with line by line comparison.
 end
